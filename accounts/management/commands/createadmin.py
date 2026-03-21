1b3376210f8e1af97e82ba1d2835ac05970f1ba5
Julius Topaz <topazjulius@gmail.com>
2026-03-21 13:11:28 +0300

    add admin creation command|

---
+++ accounts/management/commands/createadmin.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Create default admin user'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@gmail.com',
+                password='admin1234'
+            )
+            self.stdout.write(self.style.SUCCESS('Admin user created'))
+        else:
+            self.stdout.write('Admin already exists')
